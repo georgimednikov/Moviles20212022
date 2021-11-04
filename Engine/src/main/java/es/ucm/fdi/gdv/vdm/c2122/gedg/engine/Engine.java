ba127501259b9fe3fe46a985e8ada35ee3993787
Daniel González Cerdeiras <daniel20@ucm.es>
2021-11-04 18:08:23 +0100

    Actualizado Engine

--- Engine/src/main/java/es/ucm/fdi/gdv/vdm/c2122/gedg/engine/Engine.java
+++ Engine/src/main/java/es/ucm/fdi/gdv/vdm/c2122/gedg/engine/Engine.java
@@ -1,6 +1,10 @@
 package es.ucm.fdi.gdv.vdm.c2122.gedg.engine;
 
+import es.ucm.fdi.gdv.vdm.c2122.gedg.engine.Application;
+
 public interface Engine {
     Graphics getGraphics();
     Input getInput();
+    void setApplication(Application a);
+    void run();
 }
--- EngineAndroid/src/main/java/es/ucm/fdi/gdv/vdm/c2122/gedg/engineandroid/EngineAndroid.java
+++ EngineAndroid/src/main/java/es/ucm/fdi/gdv/vdm/c2122/gedg/engineandroid/EngineAndroid.java
@@ -3,15 +3,26 @@ package es.ucm.fdi.gdv.vdm.c2122.gedg.engineandroid;
 import es.ucm.fdi.gdv.vdm.c2122.gedg.engine.Engine;
 import es.ucm.fdi.gdv.vdm.c2122.gedg.engine.Graphics;
 import es.ucm.fdi.gdv.vdm.c2122.gedg.engine.Input;
+import es.ucm.fdi.gdv.vdm.c2122.gedg.engine.Application;
 
 public class EngineAndroid implements Engine {
-    @Override
-    public Graphics getGraphics() {
-        return null;
-    }
+
+    private Input input_;
+    private Graphics graphics_;
+    private Application app_;
 
     @Override
+    public void run() {  }
+    @Override
+    public Graphics getGraphics() {
+        return graphics_;
+    }
+    @Override
     public Input getInput() {
-        return null;
+        return input_;
+    }
+    @Override
+    public void setApplication(Application a) {
+        app_ = a;
     }
 }
